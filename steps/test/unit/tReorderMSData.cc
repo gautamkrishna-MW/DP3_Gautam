eb2307b771c63401fe9803085f81976d3ff0d3b0
Gautam Chavali <gautam.chavali@akkodis.com>
2024-03-19 18:10:00 +0000

    Adding BOOST_REQUIRE_EQUAL_COLLECTIONS for testcase

--- steps/test/unit/tReorderMSData.cc
+++ steps/test/unit/tReorderMSData.cc
@@ -22,14 +22,24 @@ using dp3::steps::Step;
 
 BOOST_AUTO_TEST_SUITE(reorder)
 
-// Function to compare two binary files. If the files for comparison are Meta file, the flag isMetaFile is set to true.
-bool compareBinaryFiles(const std::string& referenceFile, const std::string& inputFile, bool isMetaFile) {
-  std::ifstream refFilePtr(referenceFile, std::ifstream::ate | std::ifstream::binary);  // open file at the end
-  std::ifstream inpFilePtr(inputFile, std::ifstream::ate | std::ifstream::binary);  // open file at the end
+// Function to compare two binary files. If the files for comparison are Meta
+// file, the flag isMetaFile is set to true.
+std::pair<std::istreambuf_iterator<char>, std::istreambuf_iterator<char>>
+compareBinaryFiles(const std::string& referenceFile,
+                   const std::string& inputFile, bool isMetaFile) {
+  std::pair<std::istreambuf_iterator<char>, std::istreambuf_iterator<char>>
+      filePtrPair;
+
+  std::ifstream refFilePtr(
+      referenceFile,
+      std::ifstream::ate | std::ifstream::binary);  // open file at the end
+  std::ifstream inpFilePtr(
+      inputFile,
+      std::ifstream::ate | std::ifstream::binary);  // open file at the end
 
   if (refFilePtr.fail() || inpFilePtr.fail()) {
     std::cout << "Failed to open file" << std::endl;
-    return false;
+    return filePtrPair;
   }
 
   // Resetting file pointers to compare from the beginning.
@@ -37,25 +47,23 @@ bool compareBinaryFiles(const std::string& referenceFile, const std::string& inp
   inpFilePtr.seekg(0);  // rewind
 
   // Meta file contain path information in the first line of the binary.
-  // Since the path representation differs (i.e absolute path vs relative path), we skip the path check
-  // and compare only the rest of the meta information.
-  if (isMetaFile)
-  {
-    refFilePtr.ignore(std::numeric_limits<std::streamsize>::max(),'\n');  // Ignore first line
-    inpFilePtr.ignore(std::numeric_limits<std::streamsize>::max(),'\n');  // Ignore first line
+  // Since the path representation differs (i.e absolute path vs relative path),
+  // we skip the path check and compare only the rest of the meta information.
+  if (isMetaFile) {
+    refFilePtr.ignore(std::numeric_limits<std::streamsize>::max(),
+                      '\n');  // Ignore first line
+    inpFilePtr.ignore(std::numeric_limits<std::streamsize>::max(),
+                      '\n');  // Ignore first line
 
-    if (refFilePtr.eof() || inpFilePtr.eof())
-    {
+    if (refFilePtr.eof() || inpFilePtr.eof()) {
       std::cout << "End of file reached" << std::endl;
-      return false;
+      return filePtrPair;
     }
-  }
-  else
-  {
+  } else {
     // If file sizes are unequal return false.
     if (refFilePtr.tellg() != inpFilePtr.tellg()) {
       std::cout << "Unequal binary file sizes" << std::endl;
-      return false;  // different file size
+      return filePtrPair;  // different file size
     }
     // Resetting file pointers to compare from the beginning.
     refFilePtr.seekg(0);  // rewind
@@ -64,9 +72,11 @@ bool compareBinaryFiles(const std::string& referenceFile, const std::string& inp
 
   std::istreambuf_iterator<char> refFileIterator(refFilePtr);
   std::istreambuf_iterator<char> inpFileIterator(inpFilePtr);
-  std::istreambuf_iterator<char> endOfStream; // Default constructor is a end-of-stream iterator. 
 
-  return std::equal(refFileIterator, endOfStream, inpFileIterator);
+  filePtrPair.first = refFileIterator;
+  filePtrPair.second = inpFileIterator;
+
+  return filePtrPair;
 }
 
 // Test simple averaging without flagged points.
@@ -98,20 +108,36 @@ void test1(std::string msPath, std::string msOutPath) {
 BOOST_AUTO_TEST_CASE(testReorder1) {
   test1("../resources/midbands/reorderTestMS.ms", "./testOut.ms");
 
+  // Default stream buffer iterator has a end-of-stream state, so using this to
+  // detect end of the stream.
+  std::istreambuf_iterator<char> endOfStream;
+
   // Data File check
-  BOOST_CHECK(
+  std::pair<std::istreambuf_iterator<char>, std::istreambuf_iterator<char>>
+      filePtrPair_vis;
+  filePtrPair_vis =
       compareBinaryFiles("../resources/midbands/refTmps/reorderTestVis.tmp",
-                      "./testOut.ms-part0000-I-b0.tmp", false));
+                         "./testOut.ms-part0000-I-b0.tmp", false);
+  BOOST_REQUIRE_EQUAL_COLLECTIONS(filePtrPair_vis.first, endOfStream,
+                                  filePtrPair_vis.second, endOfStream);
 
   // Weight File check
-  BOOST_CHECK(
+  std::pair<std::istreambuf_iterator<char>, std::istreambuf_iterator<char>>
+      filePtrPair_weight;
+  filePtrPair_weight =
       compareBinaryFiles("../resources/midbands/refTmps/reorderTestWeights.tmp",
-                      "./testOut.ms-part0000-I-b0-w.tmp", false));
+                         "./testOut.ms-part0000-I-b0-w.tmp", false);
+  BOOST_REQUIRE_EQUAL_COLLECTIONS(filePtrPair_weight.first, endOfStream,
+                                  filePtrPair_weight.second, endOfStream);
 
   // Meta File check
-  BOOST_CHECK(
+  std::pair<std::istreambuf_iterator<char>, std::istreambuf_iterator<char>>
+      filePtrPair_meta;
+  filePtrPair_meta =
       compareBinaryFiles("../resources/midbands/refTmps/reorderTestMeta.tmp",
-                          "./testOut.ms-spw0-parted-meta.tmp", true));
+                         "./testOut.ms-spw0-parted-meta.tmp", true);
+  BOOST_REQUIRE_EQUAL_COLLECTIONS(filePtrPair_meta.first, endOfStream,
+                                  filePtrPair_meta.second, endOfStream);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
